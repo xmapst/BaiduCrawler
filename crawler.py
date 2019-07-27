e507cf6ada21517974425c18b3d7c4e8d8b634e6
小学课本的小明 <xmapst@gmail.com>
2019-07-27 11:42:11 +0800

    up

--- crawler.py
+++ crawler.py
@@ -21,9 +21,10 @@ from redis.connection import BlockingConnectionPool
 
 importlib.reload(sys)
 ssl._create_default_https_context = ssl._create_unverified_context
-
+# 增加redis的存储数据，存储类型hash
 Hset_name = "crawler_data"
 redis_connt = Redis(host='127.0.0.1', port=6380)
+# 动态生成headers
 UA = UserAgent()
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -34,11 +35,10 @@ headers = {
     'User-Agent': UA.chrome
 }
 
-def get_proxy():
-    return requests.get("http://118.24.52.95:5010/get/").content
-
 def downLoadHtml(word):
+    # 下载查询网页
     url = 'http://www.baidu.com.cn/s?wd=' + urllib.parse.quote(word) + '&pn='
+    # 多线程爬取，多个页面同时爬
     pool = multiprocessing.Pool(multiprocessing.cpu_count())
     for page in range(1, 10):
         result = pool.apply_async(dataDict, (url, page))
@@ -47,22 +47,26 @@ def downLoadHtml(word):
     pool.join()
  
 def dataDict(url, page):
+    # 获取每个页面的数据
     data_dict = {}
     path = url + str((page - 1) * 10)
     req = request.Request(url=path, headers=headers)
     response = request.urlopen(req)
     page = response.read()
 
+    # 解压
     gzipped = response.headers.get('Content-Encoding')
     if gzipped:
         page = zlib.decompress(page, 16+zlib.MAX_WBITS)
     else:
         page = page
-
+    
+    # 利用BeautifulSoup快速提取数据
     soup = BeautifulSoup(page, 'lxml')
     div = soup.find_all('div', attrs={'class': 'result c-container'})
     promotion = soup.find_all('a', attrs={'data-is-main-url': "true", 'data-landurl': re.compile(r"^http")})
-
+    
+    # 获取百度快照数据
     for line in div:
         data = line.find('div', attrs={'class': 'c-tools'}).get('data-tools')
         data_dict = ast.literal_eval(data)
@@ -72,20 +76,24 @@ def dataDict(url, page):
         if real_url.startswith('http'):
             data_dict['real_url'] = real_url
             data_dict['type'] = 'ordinary'
-            #redis_connt.hset(Hset_name, json.dumps(data_dict), 1)
+            # 插入快照数据
+            redis_connt.hset(Hset_name, json.dumps(data_dict), 1)
             print(data_dict)
-
+    
+    # 获取百度推广数据
     if promotion:
         for line in promotion:
             data_dict['title'] = line.get_text()
             data_dict['url'] = line.get('href')
             data_dict['real_url'] = line.get('data-landurl')
             data_dict['type'] = 'promotion'
-            #redis_connt.hset(Hset_name, json.dumps(data_dict), 1)
+            # 插入推广数据
+            redis_connt.hset(Hset_name, json.dumps(data_dict), 1)
             print(data_dict)
 
 def main():
     cn = open(keyword_dict, 'r',encoding='utf8')
+    # 循环关键字列表，对每个关键字进行查询匹配
     for line in cn:
         result = downLoadHtml(line.strip())
     cn.close()
@@ -93,6 +101,12 @@ def main():
 if __name__ == '__main__':
     global keyword_dict
     keyword_dict = 'samples.txt'
-    while True:
-        main()
-        redis_connt.hgetall(Hset_name)
+    #### 死循环查询
+    #while True:
+    #    main()
+    #    redis_connt.hgetall(Hset_name)
+    
+    #### 只查询一次
+    main()
+    redis_connt.hgetall(Hset_name)
+    
